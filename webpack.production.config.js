c99073fa9183a30b1e8dc701d7f89a3782d52be0
farhadyasir <iraz2426@gmail.com>
2019-09-18 23:09:21 +1000

    chore: Add production build

---
+++ webpack.production.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: path.resolve(__dirname, 'src/components/index.js'),
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'dist'),
+    library: 'ReactFaker',
+    libraryTarget: 'umd',
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+    modules: [path.resolve(__dirname, './src'), 'node_modules'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+  plugins: [
+  ],
+  externals: [
+    'react',
+    'react-dom',
+  ],
+};
