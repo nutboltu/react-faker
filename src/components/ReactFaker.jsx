0b81db67dcaef8a87b7afba997d0270a59f848a5
farhadyasir <iraz2426@gmail.com>
2019-10-13 17:35:12 +1100

    refactor: Remove unused codes

--- src/components/ReactFaker.jsx
+++ src/components/ReactFaker.jsx
@@ -7,7 +7,7 @@ import List from './List';
 
 // TODOS:
 // 1. Support XMLHttpRequest. Currently only support fetch
-// 2. Checkbox for skip faking
+// 2. Remove Fetch mock
 
 const containerStyle = {
   top: 20,
@@ -127,13 +127,6 @@ class ReactFaker extends React.PureComponent {
                       <RangeField  label="Latency"  />
                   </div>
                   <List items={Object.values(this.state.apiList)} onSkip={this.onSkip} />
-                  {/* <div>
-                      {
-                          Object.keys(this.state.apiList).map((key, index) => (
-                          <ApiItem  {...this.state.apiList[key]} key={index} onSkip={this.onSkip} />))
-                      }
-                  </div> */}
-
                   </div>
                   : null
               }
