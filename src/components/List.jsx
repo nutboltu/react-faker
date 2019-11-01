a25be9013a827bea97260780297c60b70af0b7e6
nutbotu <farhad.yasir.nb@gmail.com>
2019-11-01 15:37:20 +1100

    feat: change table to list

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,58 +1,57 @@
-import React from 'react';
-import Proptypes from 'prop-types';
+import React from "react";
+import Proptypes from "prop-types";
 
 const tdStyle = {
-  border: '1px solid #ddd',
-  overflow: 'hidden',
-  textOverflow: 'ellipsis',
-  padding: '12px 5px',
+  border: "1px solid #ddd",
+  overflow: "hidden",
+  textOverflow: "ellipsis",
+  padding: "12px 5px"
 };
 
 const headThStyle = {
-  paddingTop: '12px',
-  paddingBottom: '12px',
-  textAlign: 'left',
-  backgroundColor: '#527dc3',
-  color: 'white',
-  border: '1px solid #ddd',
+  paddingTop: "12px",
+  paddingBottom: "12px",
+  textAlign: "left",
+  backgroundColor: "#527dc3",
+  color: "white",
+  border: "1px solid #ddd"
 };
 
 const tableStyle = {
-  borderCollapse: 'collapse',
+  borderCollapse: "collapse"
 };
 
 const List = ({ items = [], onSkip }) => {
   return (
-    <table style={tableStyle}>
-      <thead>
-        <tr>
-          <th style={headThStyle}>On</th>
-          <th style={headThStyle}>URL</th>
-          <th style={headThStyle}>Method</th>
-        </tr>
-      </thead>
-      <tbody>
-        {items.map(({ url, method, skip }, index) => (
-          <tr key={index}>
-            <td style={tdStyle}>
-              <input
-                type="checkbox"
-                checked={!skip}
-                onChange={() => onSkip(url, method)}
-              />
-            </td>
-            <td style={tdStyle}>{url}</td>
-            <td style={tdStyle}>{method}</td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
+    <>
+      {items.map(({ url, method, skip }, index) => (
+        <details key={index}>
+          <summary>MOCK {index}</summary>
+          <div>
+            <span>Enabled</span>
+            <input
+              type="checkbox"
+              checked={!skip}
+              onChange={() => onSkip(url, method)}
+            />
+          </div>
+          <div>
+            <span>Method</span>
+            <span>{method}</span>
+          </div>
+          <div>
+            <span>URL</span>
+            <span>{url}</span>
+          </div>
+        </details>
+      ))}
+    </>
   );
 };
 
 List.propTypes = {
   items: Proptypes.array,
-  onSkip: Proptypes.func,
+  onSkip: Proptypes.func
 };
 
 export default List;
